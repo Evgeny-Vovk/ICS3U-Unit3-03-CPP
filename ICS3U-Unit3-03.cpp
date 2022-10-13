cd4a01d1b1299f136ad6b96a806f1346d0bd2afb
Evgeny Vovk <Evgeny.vovk@mths.ca>
2022-10-13 00:26:38 +0000

    done with unit3-03 cpp

---
+++ ICS3U-Unit3-03.cpp
@@ -0,0 +1,34 @@
+// Copyright(c) 2022 Evgeny Vovk All rights reserved.
+//
+// Created by : Evgeny Vovk
+// Created on : October 2022
+// ICS3U-Unit3-03.cpp File, learning if...then...else... statements in C++.
+
+#include <iostream>
+#include <random>
+
+int main() {
+    // creating variables
+    int number;
+    int guessNumber;
+
+    //random generator
+    std::random_device rseed;
+    std::mt19937 rgen(rseed());  // mersenne_twister
+    std::uniform_int_distribution<int> idist(0, 9);
+    number = idist(rgen);
+
+    // input
+    std::cout << "Guess the number from 0 to 9: ";
+    std::cin >> guessNumber;
+
+    // process and output
+    if (number == guessNumber) {
+        std::cout << "\nYou guessed right.";
+    }
+    else {
+        std::cout << "\nYou guessed wrong, the number was " << number << ".";
+    }
+
+    std::cout << "\n\n\nDone.\n";
+}
